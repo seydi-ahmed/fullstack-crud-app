c085fd1c5ada209d47dec799d55ca83bc882c92f
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-05-05 20:07:31 +0000

    création de la racine du projet

---
+++ push.sh
@@ -0,0 +1,6 @@
+git config --global user.email "seydiahmedelcheikh@gmail.com"
+git config --global user.name "seydi-ahmed"
+git config credential.helper store
+git add .
+git commit -m "création de la racine du projet"
+git push --force
