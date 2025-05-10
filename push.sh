22fcd2b6504f53e6052e9129f109e7302f84f578
seydi-ahmed <seydiahmedelcheikh@gmail.com>
2025-05-10 12:48:03 +0000

    configuratio de base terminée

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "seydi-ahmed"
 git config credential.helper store
 git add .
-git commit -m "configuratio de la base de données"
+git commit -m "configuratio de base terminée"
 git push --force
